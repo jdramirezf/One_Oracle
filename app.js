bef1c67eeb4feb391b9a5f40a11b5de5cec5fce4
Juan David Ramirez Farias <jdramirezf@gmail.com>
2025-02-13 01:16:32 -0500

    Actualización de funciones de validación de condiciones del juego

--- app.js
+++ app.js
@@ -1,15 +1,36 @@
 
+let numeroSecreto = generarNumeroSecreto();
+
 function asignacionTextoElemento(elemento, texto) {
     let elementoHTML = document.querySelector(elemento);
     elementoHTML.innerHTML = texto;
+    return;
 }
 
+function generarNumeroSecreto() {
+    return Math.floor(Math.random() * 10) + 1;
+}
 
 function intentoUsuario() {
-    alert('Click desde el botón');
+    let numeroDelUsuario = parseInt(document.getElementById('NUMERO').value);
+    /*console.log(typeof(numeroDelUsuario));
+    console.log(typeof(numeroSecreto));
+    console.log(numeroSecreto);
+    console.log(numeroDelUsuario);
+    console.log(numeroSecreto === numeroDelUsuario);*/         
+    if (numeroSecreto === numeroDelUsuario) {
+        asignacionTextoElemento('#PARRAFO', '¡Felicidades! Has adivinado el número secreto');
+    } else {
+        if (numeroDelUsuario > numeroSecreto) { 
+            asignacionTextoElemento('#PARRAFO', '¡Lo siento! El número secreto es menor');
+           }
+            else {
+                asignacionTextoElemento('#PARRAFO', '¡Lo siento! El número secreto es mayor');
+            }
+        }
+    
+    return;
 }
-
-
 
 asignacionTextoElemento('#TITULO', 'JUEGO DEL NUMERO SECRETO');
 asignacionTextoElemento('#PARRAFO', 'Indica un número del 1 al 10');
