320369cef151917f0d0e375d5dd60ac210c35b49
Juan David Ramirez Farias <jdramirezf@gmail.com>
2025-02-13 00:08:00 -0500

    Agregar archivo app.js al proyecto

---
+++ app.js
@@ -0,0 +1,12 @@
+let titulo = document.querySelector('#TITULO');
+titulo.innerHTML = 'Juego del número secreto';
+console.log(titulo);
+
+let parrafo = document.querySelector('#PARRAFO');
+parrafo.innerHTML = 'Indica un número del 1 al 10';
+console.log(parrafo);
+
+function intentoUsuario() {
+    alert('Click desde el botón');
+}
+
