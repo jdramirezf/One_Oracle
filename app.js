5d54c6c6fc7465b8bffd5d589ccc42f7bb002849
Juan David Ramirez Farias <jdramirezf@gmail.com>
2025-02-13 00:34:48 -0500

    Actualización de la información en nuestro proyecto

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
-let titulo = document.querySelector('#TITULO');
-titulo.innerHTML = 'Juego del número secreto';
-console.log(titulo);
 
-let parrafo = document.querySelector('#PARRAFO');
-parrafo.innerHTML = 'Indica un número del 1 al 10';
-console.log(parrafo);
+function asignacionTextoElemento(elemento, texto) {
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+}
+
 
 function intentoUsuario() {
     alert('Click desde el botón');
 }
+
+
+
+asignacionTextoElemento('#TITULO', 'JUEGO DEL NUMERO SECRETO');
+asignacionTextoElemento('#PARRAFO', 'Indica un número del 1 al 10');
+asignacionTextoElemento('#PARRAFO2', 'Esto es un mensaje de prueba para el segundo párrafo');
 
